eaf4e924dad570b351eabedec31279189246fa4c
rszawlow <radek.szawlowski10@gmail.com>
2023-09-06 13:14:13 +0200

    poprawki

--- obiad_project/obiad/admin.py
+++ obiad_project/obiad/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import MealChoice
 
 
+
 class MealChoiceAdmin(admin.ModelAdmin):
     list_display = ['user', 'data', 'sniadanie', 'obiad', 'kolacja']
     list_filter = ['user', 'data']
